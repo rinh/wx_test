136ef2f7ecfdd99aada26441446ddb69c8e1ba32
rinh <lhqunar@gmail.com>
2019-04-26 15:26:59 +0800

    add

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
+const request_origin = require('request')
+const request = require('request-promise')
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 80
 
-app.get('/', (req, res) => res.send('Hello World!'))
+const appID = "wx7d7c1ee9a4a012b0";
+const appSecret = "2a14d5c586b8e301814caad56202cd0c";
+
+app.get('/get_token', (req, res) => {
+
+    request(`https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=${appID}&secret=${appSecret}`)
+    .then((body) => {
+        res.send( body )
+    })
+
+});
+
+app.get('wx_test',(req,res) => {
+    req.param
+
+
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
